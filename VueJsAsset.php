69107c23aab9cfe5b7c37e84c25fd04da868959a
= <joao.cunha@webcomum.com>
2018-03-01 21:46:14 +0000

    uopdated Vue, added assets, updated composer.json

--- VueJsAsset.php
+++ VueJsAsset.php
@@ -1,22 +1,15 @@
 <?php
-/**
- * @link http://www.konkeanweb.com
- * 1/24/2017 AD 5:30 PM
- * @copyright Copyright (c) 2017 served
- * @author Prawee Wongsa <konkeanweb@gmail.com>
- * @license BSD-3-Clause
- */
+
 namespace crawlingcity\vuejs;
 
 use yii\web\AssetBundle;
 
 class VueJsAsset extends AssetBundle
 {
-    public $sourcePath = '@bower/vue/dist';
-
     public function init()
     {
         parent::init();
+        $this->sourcePath = __DIR__ . '/assets/';
         $this->js[] = YII_ENV_DEV ? 'vue.js':'vue.min.js';
     }
 
